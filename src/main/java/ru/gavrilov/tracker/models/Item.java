77cd4b808ff63fda918574af340f34a68f891827
Frechman <31288209+Frechman@users.noreply.github.com>
2018-07-28 17:04:41 +0300

    Minor change entity Item.

--- src/main/java/ru/gavrilov/tracker/models/Item.java
+++ src/main/java/ru/gavrilov/tracker/models/Item.java
@@ -1,42 +1,50 @@
 package ru.gavrilov.tracker.models;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Date;
 
-@Entity
-@AllArgsConstructor
-@EqualsAndHashCode
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 @Getter
 @Setter
+
+@Entity
+@Table(name = "ITEM")
 public class Item implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
+    @Column(name = "NAME", nullable = false)
     private String name;
 
+    @Column(name = "DESCRIPTION", nullable = false)
     private String description;
 
+    @Column(name = "DATE", nullable = false)
     @Temporal(value = TemporalType.TIMESTAMP)
     private Date dateCreate;
 
-    public Item() {
+    public Item(String name, String description, Date dateCreate) {
+        this.name = name;
+        this.description = description;
+        this.dateCreate = dateCreate;
     }
 
     @Override
     public String toString() {
-        return "Item{" +
+        return "Item[" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 ", dateCreate=" + dateCreate +
-                '}';
+                ']';
     }
 }
