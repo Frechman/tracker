94a02f29092ebeb8f2ecf046a41688206862cd01
dev <serj.g13@yandex.ru>
2018-08-03 17:05:16 +0300

    Minor changes webui.

--- src/main/java/ru/gavrilov/tracker/WebConfig.java
+++ src/main/java/ru/gavrilov/tracker/WebConfig.java
@@ -6,14 +6,13 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
-//        registry.addViewController("/items/all").setViewName("listItems");
-//        registry.addViewController("/items/add").setViewName("addItem");
-//        //registry.addViewController("/items/{id}").setViewName("showItem");
-//        registry.addViewController("/items/search").setViewName("search");
+        registry.addViewController("/items/all").setViewName("listItems");
+        registry.addViewController("/items/add").setViewName("addItem");
+        registry.addViewController("/items/{id}").setViewName("showItem");
+        registry.addViewController("/items/search").setViewName("search");
     }
 }
